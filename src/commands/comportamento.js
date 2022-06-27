066147ee61474ccacedb8f48612e9e6895869535
Cahian Freire <cahianfreire@gmail.com>
2022-06-27 09:31:56 -0300

    wip

--- src/commands/comportamento.js
+++ src/commands/comportamento.js
@@ -9,119 +9,119 @@ import { createSlashCommand }
 
 const database = new Database(config.replit.databaseUrl);
 
-const data = createSlashCommand("./comportamento.json");
+const command = {
+  data: createSlashCommand("./comportamento.json");
 
-async function receive(message) {
-  if (message.author.bot) {
-    return;
-  }
-
-  const { submundoChat, submundoHumorNegro } = config.discord.channels;
-  if (
-    message.channelId == submundoChat.id ||
-    message.channelId == submundoHumorNegro.id
-  ) {
-    return;
-  }
-  let data = await database.get("usersData");
-  let user = data["users"][message.author.id];
-
-  if (!user) {
-    data["users"][message.author.id] = {
-      id: message.author.id,
-      name: `${message.author.username}`,
-      img: message.author.avatarURL(),
-      badwords: {},
-    };
-    await database.set("usersData", data);
-    data = await database.get("usersData");
-    user = data["users"][message.author.id];
-  }
-  let check = message.content;
-  for (word of badwords) {
-    let rgx = new RegExp("\\b" + remove(word) + "\\b", "ig");
-    let bol = remove(check).match(rgx);
-    if (bol) {
-      message.client.channels.cache
-        .find((ch) => ch.id == channelIds.report)
-        .send(
-          `Atenção! Um usuário disse uma palavra proibida: \n Nome: ${message.author.username} \n Id: ${message.author.id} \n Mensagem: ${message.content} \n Link: ${message.url}`
-        );
-      if (!user.badwords[word]) {
-        data["users"][user["id"]].badwords[word] = 1;
-        await database.set("usersData", data);
-      } else {
-        data["users"][user["id"]].badwords[word]++;
-        await database.set("usersData", data);
-      }
+  async function receive(message) {
+    if (message.author.bot) {
+      return;
     }
-  }
-}
 
-async function execute(interaction) {
-  const action = await interaction;
-  const adm = interaction.member.roles.cache.get("864720804691050496");
-  const mod = interaction.member.roles.cache.get("959787387184107580");
-  let data = await database.get("usersData");
-  let users = data["users"];
-  const id = action.options.getString("id");
-  const bool = action.options.getBoolean("mostrar");
-  if (users[id]) {
-    let wordsNumber = Object.values(users[id]["badWords"]);
-    if (wordsNumber[0]) {
-      wordsNumber = wordsNumber.reduce((i, j) => {
-        return i + j;
-      }, 0);
-    } else {
-      wordsNumber = 0;
+    const { submundoChat, submundoHumorNegro } = config.discord.channels;
+    if (
+      message.channelId == submundoChat.id ||
+      message.channelId == submundoHumorNegro.id
+    ) {
+      return;
     }
-    let color;
-    const embed = new MessageEmbed()
-      .setThumbnail(users[id].img)
-      .setAuthor(users[id].name)
-      .setDescription(
-        wordsNumber < 10
-          ? (() => {
-              color = "#ffffff";
-              return "Usuário comportado. Parabéns! :) \n O número de palavras ofensivas ditas é: **Abaixo de 10!**";
-            })()
-          : wordsNumber < 50
-          ? (() => {
-              color = "#facc41";
-              return "Opa! Percebemos um número um pouco frequente de palavras ofensivas ditas nos canais livres, vamos maneirar!";
-            })()
-          : (() => {
-              color = "#f00000";
-              return "Você parecer ter um comportamento um pouco excessivo... Caso haja denúncias, o seu histórico poderá influenciar na sua punição.";
-            })()
-      );
-    embed.setColor(color);
-    if ((bool && adm) || (bool && mod)) {
-      let list = Object.entries(users[id].badWords),
-        words = "";
-      if (list[0]) {
-        for (item of list) {
-          words += item[0] + ": " + item[1] + "\n";
+    let data = await database.get("usersData");
+    let user = data["users"][message.author.id];
+
+    if (!user) {
+      data["users"][message.author.id] = {
+        id: message.author.id,
+        name: `${message.author.username}`,
+        img: message.author.avatarURL(),
+        badwords: {},
+      };
+      await database.set("usersData", data);
+      data = await database.get("usersData");
+      user = data["users"][message.author.id];
+    }
+    let check = message.content;
+    for (word of badwords) {
+      let rgx = new RegExp("\\b" + remove(word) + "\\b", "ig");
+      let bol = remove(check).match(rgx);
+      if (bol) {
+        message.client.channels.cache
+          .find((ch) => ch.id == channelIds.report)
+          .send(
+            `Atenção! Um usuário disse uma palavra proibida: \n Nome: ${message.author.username} \n Id: ${message.author.id} \n Mensagem: ${message.content} \n Link: ${message.url}`
+          );
+        if (!user.badwords[word]) {
+          data["users"][user["id"]].badwords[word] = 1;
+          await database.set("usersData", data);
+        } else {
+          data["users"][user["id"]].badwords[word]++;
+          await database.set("usersData", data);
         }
-      } else {
-        words = "No words!";
       }
-      embed.addFields({
-        name: "Lista de Palavras:",
-        value: words,
-      });
-      action.reply({ embeds: [embed] });
-    } else {
-      action.reply({ embeds: [embed] });
     }
-  } else {
-    action.reply({
-      content: "Nenhum usuário com este ID.",
-      ephemeral: true,
-    });
   }
-}
 
-const command = { data, receive, execute };
+  async function execute(interaction) {
+    const action = await interaction;
+    const adm = interaction.member.roles.cache.get("864720804691050496");
+    const mod = interaction.member.roles.cache.get("959787387184107580");
+    let data = await database.get("usersData");
+    let users = data["users"];
+    const id = action.options.getString("id");
+    const bool = action.options.getBoolean("mostrar");
+    if (users[id]) {
+      let wordsNumber = Object.values(users[id]["badWords"]);
+      if (wordsNumber[0]) {
+        wordsNumber = wordsNumber.reduce((i, j) => {
+          return i + j;
+        }, 0);
+      } else {
+        wordsNumber = 0;
+      }
+      let color;
+      const embed = new MessageEmbed()
+        .setThumbnail(users[id].img)
+        .setAuthor(users[id].name)
+        .setDescription(
+          wordsNumber < 10
+            ? (() => {
+                color = "#ffffff";
+                return "Usuário comportado. Parabéns! :) \n O número de palavras ofensivas ditas é: **Abaixo de 10!**";
+              })()
+            : wordsNumber < 50
+            ? (() => {
+                color = "#facc41";
+                return "Opa! Percebemos um número um pouco frequente de palavras ofensivas ditas nos canais livres, vamos maneirar!";
+              })()
+            : (() => {
+                color = "#f00000";
+                return "Você parecer ter um comportamento um pouco excessivo... Caso haja denúncias, o seu histórico poderá influenciar na sua punição.";
+              })()
+        );
+      embed.setColor(color);
+      if ((bool && adm) || (bool && mod)) {
+        let list = Object.entries(users[id].badWords),
+          words = "";
+        if (list[0]) {
+          for (item of list) {
+            words += item[0] + ": " + item[1] + "\n";
+          }
+        } else {
+          words = "No words!";
+        }
+        embed.addFields({
+          name: "Lista de Palavras:",
+          value: words,
+        });
+        action.reply({ embeds: [embed] });
+      } else {
+        action.reply({ embeds: [embed] });
+      }
+    } else {
+      action.reply({
+        content: "Nenhum usuário com este ID.",
+        ephemeral: true,
+      });
+    }
+  }
+};
 
 export default command;
--- src/commands/diario.js
+++ src/commands/diario.js
@@ -4,102 +4,98 @@ import * as linkify from "linkifyjs";
 import { createSlashCommand } from "../builder.js";
 import config from "../../config.js";
 
-function createMessageEmbed(options, user) {
-  let url = new URL(options.getString("imagem"));
-  let autor = "";
-  if (options.getString("autor")) {
-    autor = " - Autor: " + options.getString("autor");
-  }
+function createMessageEmbed(user) {
+  const { options } = user;
 
-  const exampleEmbed = new MessageEmbed()
-    .setColor("#b80000")
-    .setTitle(options.getString("título"))
-    .setDescription(options.getString("descrição"))
-    .setThumbnail(
-      "https://cdn.discordapp.com/attachments/844699066930954302/968664576013004861/Tragi.png"
-    )
+  // TODO: Check if Image and URL are valid URLs.
+
+  const colors = {
+    undefined: "#b80000",
+    vermelho: "#ed0000",
+    laranja: "#ff8000",
+    amarelo: "#fff200",
+    verde: "#48ff00",
+    azul: "#005eff",
+    roxo: "#a600ed",
+    rosa: "#ff00f7",
+    branco: "#ffffff",
+    preto: "#000000",
+  };
+
+  const messageEmbed = new MessageEmbed()
+    .setURL(options.getString("fonte")
+    .setTitle(options.getString("titulo"))
+    .setDescription(options.getString("descricao"))
     .setImage(options.getString("imagem"))
-    .setTimestamp()
-    .setFooter({
-      text: "Publicado por: " + user.username + autor,
-      iconURL: user.avatarURL(),
+    .setColor(colors[options.get("cor")])
+    .setThumbnail("./images/tragidaily.png")
+    .setTimestamp();
+
+  const subtitulo = options.getString("subtitulo");
+  const subdescricao = options.getString("subdescricao");
+
+  if (subtitulo) {
+    messageEmbed.addFields({
+      name: subtitulo,
+      value: subdescricao || "-",  // NOTE: Deveria ser um traço?
     });
-
-  let subtitulo = options.getString("subtitulo"),
-    subdescrição = options.getString("subdescrição");
-  let fonte = options.getString("fonte"),
-    cor = options.getString("cor");
-
-  if (fonte) {
-    let url2 = new URL(fonte);
-    exampleEmbed.setURL(fonte);
   }
 
-  if (cor) {
-    let cores = {
-      vermelho: "#ed0000",
-      laranja: "#ff8000",
-      amarelo: "#fff200",
-      verde: "#48ff00",
-      azul: "#005eff",
-      roxo: "#a600ed",
-      rosa: "#ff00f7",
-      branco: "#ffffff",
-      preto: "#000000",
-    };
-    if (cores[cor]) {
-      exampleEmbed.setColor(cores[cor]);
+  const autor = options.get("autor");
+  const footerPublicador = `Publicado por: ${user.username}`
+  const footerAutor = autor ? ` - Autor: ${autor}` : "";
+
+  messageEmbed.setFooter({
+    text: `${footerPublicador}${footerAutor}`,
+    iconURL: user.avatarURL(),
+  })
+
+  return messageEmbed;
+}
+
+const command = {
+  data: createSlashCommand("./diario.json");
+
+  async function receive(message) {
+    const { militadas, artes } = config.discord.channels;
+    const { channelId, attachments, content } = message;
+
+    if (
+      (channelId == militadas.id || channelId == artes.id) &&
+      (!attachments.size && !linkify.test(content, "url"))
+    ) {
+      setTimeout(() => message.delete(), 1000);
     }
   }
 
-  if (subtitulo) {
-    let field = { name: subtitulo, value: "-" };
-    if (subdescrição) field["value"] = subdescrição;
-    exampleEmbed.addFields(field);
-  }
+  async function execute(interaction) {
+    const { channels, roles } = config.discord;
+    const { client, member, user } = interaction;
 
-  return exampleEmbed;
-}
-
-const data = createSlashCommand("./diario.json");
-
-async function receive(message) {
-  const { militadas, artes } = config.discord.channels;
-  const { channelId, attachments, content } = message;
-
-  if (
-    (channelId == militadas.id || channelId == artes.id) &&
-    (!attachments.size && !linkify.test(content, "url"))
-  ) {
-    setTimeout(() => message.delete(), 1000);
-  }
-}
-
-async function execute(interaction) {
-  const { channels, roles } = config.discord;
-  const { client, member } = interaction;
-
-  try {
-    const role = member.roles.cache.get(roles.jornalistaInvestigativo);
+    const roleId = roles.jornalistaInvestigativo.id;
+    const role = member.roles.cache.get(roleId);
     const channelId = role ? channels.diario.id : channels.journal.id;
     const channel = client.channels.cache.get(channelId);
-    const embed = createMessageEmbed();
 
-    channel.send({ embeds: [exampleEmbed] });
+    try {
+      const messageEmbed = createMessageEmbed(user);
 
-    await interaction.reply({
-      content: "Sua noticia foi publicada!",
-      ephemeral: false,
-    });
-    interaction.deleteReply(); // NOTE: Why deleteReply after reply?!?!?
-  } catch (error) {
-    await interaction.reply({
-      content: "URL invalido. Tente novamente!",
-      ephemeral: true,
-    });
+      channel.send({ embeds: [messageEmbed] });
+
+      await interaction.reply({
+        content: "Sua noticia foi publicada!",
+        ephemeral: false, // NOTE: true or false?
+      });
+      // NOTE(Virgulas): Why deleteReply after reply?
+      // interaction.deleteReply();
+    } catch (error) {
+      // TODO: This error will always be because an invalid URL? NO!!!
+      await interaction.reply({
+        content: "URL invalido. Tente novamente!",
+        ephemeral: true,
+      });
+    }
   }
-}
-
-const command = { data, receive, execute };
+};
 
 export default command;
